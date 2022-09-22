e105a41b431850b889fbeff8849a42ac29d421f1
Kenson9999 <kensonchan38@gmail.com>
2022-09-23 00:05:59 +0800

    updateSrc2

---
+++ src 2/app/tab2/tab2.module.ts
@@ -0,0 +1,43 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Tab2Page } from './tab2.page';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
+import { Tab2PageRoutingModule } from './tab2-routing.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: Tab2Page,
+    children: [
+      {
+        path: 'user',
+        loadChildren: ()=>
+        import('../user/user.module').then(m => m.UserPageModule)
+      },
+      {
+        path: 'account',
+        loadChildren: ()=>
+        import('../account/account.module').then(m => m.AccountPageModule)
+      },
+      {
+        path: '',
+        redirectTo: 'user',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [Tab2Page]
+})
+export class Tab2PageModule {}
